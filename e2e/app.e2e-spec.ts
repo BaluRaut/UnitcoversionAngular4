059a5a8caa7731de5f0003ccffc6460a4f6a2475
balusraut@gmail.com <balusraut@gmail.com>
2017-08-09 07:33:23 +0530

    Intial Setup

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TwodegreePage } from './app.po';
+
+describe('twodegree App', () => {
+  let page: TwodegreePage;
+
+  beforeEach(() => {
+    page = new TwodegreePage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
